cb65cec18b22c566f3c871f5f475f0dcc2e4bc44
durpy <89787577+durpyneko@users.noreply.github.com>
2025-08-01 16:39:27 +0200

    feat: set title for /about

--- src/pages/about.rs
+++ src/pages/about.rs
@@ -1,9 +1,12 @@
 use crate::rtml;
+use crate::utils::snippets;
 
 pub fn render() -> web_sys::Element {
+    snippets::set_title("About / Melancholy");
+
     let tree = rtml! {
         h1 {
-            "about page"
+            "about page here"
         }
     };
 
