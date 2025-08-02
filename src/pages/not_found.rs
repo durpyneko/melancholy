26ad2cc350e3e547cfd04c6e8a8c96782833838f
durpy <89787577+durpyneko@users.noreply.github.com>
2025-08-03 01:18:37 +0200

    style: make return more apparent

--- src/pages/not_found.rs
+++ src/pages/not_found.rs
@@ -6,14 +6,21 @@ pub fn render() -> web_sys::Element {
             class: "flex items-center justify-center h-screen"
             a {
                 href: "/"
-                class: "flex flex-col gap-8"
-                span {
-                    class: "text-3xl flex justify-center"
-                    "Page not found"
-                }
                 img {
                     class: "w-[300px]"
                     src: "/images/d722262e277bb21e40d36ec5f2f5c773.png"
+                }
+                class: "flex flex-col gap-8"
+                div {
+                    class: "flex flex-col gap-2"
+                    span {
+                        class: "text-3xl flex justify-center"
+                        "Page not found"
+                    }
+                    span {
+                        class: "text-3xl flex justify-center bold text-[#366]"
+                        "[return]"
+                    }
                 }
             }
         }
